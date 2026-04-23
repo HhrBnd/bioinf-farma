5f299d2af0f9b38baaf1699a57223da65205495b
Heather <149694694+HhrBnd@users.noreply.github.com>
2026-04-23 10:46:26 +0200

    Update copyright information in stability_prediction.py

--- script/stability_prediction.py
+++ script/stability_prediction.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
-# Copyright (C) 2026 [Your Name / Organization]
+# Copyright (C) 2023-2026 Heather Bondi, Gianluca Molla, Università degli Studi dell'Insubria
 # -*- coding: utf-8 -*-
 
 import sys
